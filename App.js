9af46fcc3feab343e2729220e8328d3952e800d4
Filip Brej <brej.filip@gmail.com>
2020-10-12 21:30:52 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Filip');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter name:</Text>
+      <TextInput keyboardType='name-phone-pad' style={styles.input} placeholder="enter a name" onChangeText={(val) => setName(val)}/>
+      <Text>His name is {name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    margin: 20,
+    borderWidth: 1,
+    borderColor: '#888',
+    borderRadius: 20,
+    padding: 10,
+    width: 200
+  }
 });
